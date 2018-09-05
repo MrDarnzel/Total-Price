6b6f6ecdd435a28fa48889fe6a381959710a847c
MrDarnzel <42988374+MrDarnzel@users.noreply.github.com>
2018-09-05 15:15:16 -0700

    Delete Celcius.py

--- Celcius.py
+++
@@ -1,7 +0,0 @@
-Temp_Celcius = input(" What is the Temperature in Celcius? ") #Asks the user for Temperature in Celcius
-float(Temp_Celcius)  #Converts to Float
-print(Temp_Celcius)  #Prints Celcius Temperature
-
-
-Temp_Fahrenheit = (((float(Temp_Celcius) * 9) / 5) + 32)            #Takes C and converts into F
-print(" The temperature in Fahrenheit is " + str(Temp_Fahrenheit))  #Prints your temperature
